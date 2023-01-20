301e709ad23bfe8e4ecc9c8c9aa6a210bd9b8823
Ivar Maximiliano Dacrema <ivar.m.dacrema@hotmail.com>
2023-01-20 13:38:22 -0300

    Primera entrega finalizada creo

--- index.js
+++ index.js
@@ -11,18 +11,33 @@ class ProductManager {
 
     addProduct(title,description,price,thumbnail,code,stock){
         const product = {
-            id: this.#generarId(),
+            id: this.#generateId(),
             title,
             description,
             price,
             thumbnail,
             code,
             stock
-    }
-        this.products.push(product)
+        }
+        if(title && description && price && thumbnail && code && stock){
+            this.products.push(product)
+        } else {
+            console.log(`En el producto ${title}, uno de los campos no fue rellenado`)
+        }
+        
     }
 
-    #generarId(){
+    getProductById(idProduct){
+        const product = this.#searchProduct(idProduct)
+        if(product===undefined){
+            console.log(`El producto no se encuentra en la lista`)
+        }else{
+            console.log(`El producto esta en la lista, es ${product.title}.`)
+            console.log(product)
+        }
+    }
+
+    #generateId(){
         let id = 1
         if(this.products.length!==0){
             id = this.products[this.products.length-1].id + 1
@@ -30,13 +45,16 @@ class ProductManager {
         return id
     }
 
-    #encontrarProducto(idProduct){
+    #searchProduct(idProduct){
         return this.products.find(product=>product.id===idProduct)
     }
 }
 
 const productManager1 = new ProductManager()
 productManager1.addProduct('leche','lacteo de gran calidad',250,1,153645,12)
+productManager1.addProduct('galletas','pasta con chocolate al horno',500,2,155645,52)
+productManager1.addProduct('helado', 'frio como la mier...', 100, 5, 76766)
 
+productManager1.getProductById(5)
 
 console.log(productManager1.getProduct());
