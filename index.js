3fbd7a005ecda74a3c7fec8dcad59a5a8fde37ca
Ivar Maximiliano Dacrema <ivar.m.dacrema@hotmail.com>
2023-01-21 00:08:42 -0300

    agregando funcionalidad

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ class ProductManager {
     // thumbnail: ruta de imagen-code: codigo identificador
 
     addProduct(title,description,price,thumbnail,code,stock){
+        const controlCode = this.#searchCode(code)
+        console.log(controlCode)
         const product = {
             id: this.#generateId(),
             title,
@@ -19,10 +21,13 @@ class ProductManager {
             code,
             stock
         }
-        if(title && description && price && thumbnail && code && stock){
-            this.products.push(product)
-        } else {
+        if(!title || !description || !price || !thumbnail || !code || !stock){
             console.log(`En el producto ${title}, uno de los campos no fue rellenado`)
+        } else if (controlCode) {
+            const product = this.products.find(product=>product.code===code)
+            console.log(`${title} ya se encuentra en la lista con el codigo ${product.code}`)
+        }  else {
+            this.products.push(product)
         }
         
     }
@@ -48,13 +53,20 @@ class ProductManager {
     #searchProduct(idProduct){
         return this.products.find(product=>product.id===idProduct)
     }
+
+    #searchCode(code){
+        return this.products.includes(code)
+    }
 }
 
 const productManager1 = new ProductManager()
 productManager1.addProduct('leche','lacteo de gran calidad',250,1,153645,12)
 productManager1.addProduct('galletas','pasta con chocolate al horno',500,2,155645,52)
-productManager1.addProduct('helado', 'frio como la mier...', 100, 5, 76766)
+productManager1.addProduct('helado', 'crema fria con diferentes gustos', 100, 5, 76766, 4)
+productManager1.addProduct('hamburguesas', 'medallon de carne molida', 100, 5, 76767,100)
+productManager1.addProduct('helado', 'crema fria con diferentes gustos', 100, 5, 76766)
 
 productManager1.getProductById(5)
 
 console.log(productManager1.getProduct());
+
